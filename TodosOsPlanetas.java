54ae8bc3e6d3b510283fd4a2d96e8a77b870b11f
Jessica Roecker Arns <108406139+JessicaRoecker@users.noreply.github.com>
2023-03-12 19:22:42 -0300

    Initial commit

---
+++ TodosOsPlanetas.java
@@ -0,0 +1,8 @@
+public class TodosOsPlanetas {
+    public Planeta[] getPlanetas() {
+        Planeta[] planetas = { new Mercurio(), new Venus(), new Terra(), new Marte(), new Jupiter(), new Saturno(), new Urano(), new Netuno()};
+
+        return planetas;
+    }
+}
+
